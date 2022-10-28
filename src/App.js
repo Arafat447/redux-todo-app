f2b545578738d8b1d54055972526427eb5441af0
Arafat447 <tahmidhasan184@gmail.com>
2022-10-28 18:06:23 +0600

    redux integrated

--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
-import Navigation from './Components/Navigation';
-import Header from './Components/Header';
-import TodoList from './Components/TodoList';
-import Footer from './Components/Footer';
+import logo from "./logo.svg";
+import "./App.css";
+import Navigation from "./Components/Navigation";
+import Header from "./Components/Header";
+import TodoList from "./Components/TodoList";
+import Footer from "./Components/Footer";
+import { Provider } from "react-redux";
+import store from "./Redux/store";
 
 function App() {
   return (
-    <div
-            className="grid place-items-center bg-blue-100 h-screen px-6 font-sans"
-        >
-            <Navigation/>
-            <div className="w-full max-w-3xl shadow-lg rounded-lg p-6 bg-white">
-                <Header/>
-                <hr className="mt-4" />
+    <Provider store={store}>
+      <div className="grid place-items-center bg-blue-100 h-screen px-6 font-sans">
+        <Navigation />
+        <div className="w-full max-w-3xl shadow-lg rounded-lg p-6 bg-white">
+          <Header />
+          <hr className="mt-4" />
 
-                <TodoList/>
-                
+          <TodoList />
 
-                <hr className="mt-4" />
+          <hr className="mt-4" />
 
-                <Footer/>
-                
-            </div>
+          <Footer />
         </div>
+      </div>
+    </Provider>
   );
 }
 
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,16 +1,37 @@
-import React from "react";
-import clickImage from "../asset/images/double-tick.png"
-import notesImage from "../asset/images/notes.png"
-import plusImage from "../asset/images/plus.png"
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import clickImage from "../asset/images/double-tick.png";
+import notesImage from "../asset/images/notes.png";
+import plusImage from "../asset/images/plus.png";
+import { added,completedAll,clearCompleted } from "../Redux/todos/actions";
 const Header = () => {
+  const dispatch = useDispatch()
+  const [input, setInput] = useState("");
+  const handleSetInput = (e) => {
+    setInput(e.target.value);
+  };
+  const handleSubmitInput = (e) => {
+    e.preventDefault();
+    dispatch(added(input))
+    setInput("")
+  };
+  const handleAllCompleted = (e) => {
+    dispatch(completedAll())
+  }
+  const handleClearCompleted = (e) => {
+    dispatch(clearCompleted())
+  }
   return (
     <div>
-      <form className="flex items-center bg-gray-100 px-4 py-4 rounded-md">
+      <form onSubmit={handleSubmitInput} className="flex items-center bg-gray-100 px-4 py-4 rounded-md">
         <img src={notesImage} className="w-6 h-6" alt="Add todo" />
         <input
           type="text"
           placeholder="Type your todo"
           className="w-full text-lg px-4 py-1 border-none outline-none bg-gray-100 text-gray-500"
+          onChange={handleSetInput}
+          value={input}
+          required
         />
         <button
           type="submit"
@@ -19,15 +40,11 @@ const Header = () => {
       </form>
 
       <ul className="flex justify-between my-4 text-xs text-gray-500">
-        <li className="flex space-x-1 cursor-pointer">
-          <img
-            className="w-4 h-4"
-            src={clickImage}
-            alt="Complete"
-          />
+        <li className="flex space-x-1 cursor-pointer" onClick={handleAllCompleted}>
+          <img className="w-4 h-4" src={clickImage} alt="Complete" />
           <span>Complete All Tasks</span>
         </li>
-        <li className="cursor-pointer">Clear completed</li>
+        <li className="cursor-pointer" onClick={handleClearCompleted}>Clear completed</li>
       </ul>
     </div>
   );
